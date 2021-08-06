291d74558709ab910af9f14bbc88163f4cd0c8e2
robin - 108 master <s103321048@mail1.ncnu.edu.tw>
2021-08-06 16:07:27 +0800

    add .gitignore

---
+++ model_coverage.py
@@ -0,0 +1,213 @@
+from transformers.tokenization_bert import BertTokenizer
+from transformers.modeling_bert import BertForMaskedLM
+from torch.nn.modules.loss import CrossEntropyLoss
+import torch, os, time, tqdm, numpy as np, h5py
+
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
+from collections import Counter
+import joblib
+from util_mydep import mydep_dict
+
+class KeywordExtractor():
+    def __init__(self, n_kws=15):
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.n_kws = n_kws
+
+    #     self.bert_w2i = {w: i for i, w in enumerate(self.tokenizer.vocab)}
+    #     self.bert_vocab = self.tokenizer.vocab
+    #     # self.dataset = h5py.File("/home/phillab/data/headliner_6M.hdf5")
+    #     # self.dset = self.dataset['name']
+    #     self.keyworder = None
+    #     self.i2w = None
+    #     # self.cache = {}
+    #     # self.cache_keys = []
+
+    # def train(self):
+    #     stop_words = ["'", ".", "!", "?", ",", '"', '-', 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+    #     stop_indices = set([bert_w2i[w] for w in stop_words if w in bert_w2i])
+    #     dv = DictVectorizer()
+    #     tt = TfidfTransformer()
+    #     self.keyworder = Pipeline([('counter', dv), ('tfidf', tt)])
+
+    #     def remove_stop_words(tokenized):
+    #         return [w for w in tokenized if w not in stop_indices]
+
+    #     N = 100000
+    #     text_inputs = [self.tokenizer.encode(dset[i][0].decode()) for i in tqdm.tqdm_notebook(range(N))] # Tokenize
+    #     text_inputs = [remove_stop_words(text) for text in text_inputs] # Remove stop words
+
+    #     text_inputs = [Counter(text) for text in text_inputs] # Make a Count dictionary
+    #     training_output = self.keyworder.fit_transform(text_inputs)
+
+    # def save(self, outfile):
+    #     joblib.dump(self.keyworder, outfile)
+
+    # def reload(self, infile):
+    #     self.keyworder = joblib.load(infile)
+    #     self.counter = self.keyworder.named_steps['counter']
+    #     self.i2w = {i:w for w,i in self.counter.vocabulary_.items()}
+
+    def extract_keywords(self, content):
+        # # if text in self.cache:
+        # #     return self.cache[text]
+
+        # # unmasked = self.tokenizer.encode(text)
+        # tfidf = self.keyworder.transform([Counter(unmasked)])
+        # kws = np.argsort(tfidf.toarray()[0])[::-1][:self.n_kws]
+        # kws_is = [self.i2w[kw] for kw in kws]
+        # kws_texts = [self.tokenizer.ids_to_tokens[kwi] for kwi in kws_is]
+        # # print(kws_is, kws_texts)
+        # outputs = (kws_is, kws_texts)
+
+        # # if len(self.cache) > 1000:
+        # #     del self.cache[self.cache_keys.pop(0)]
+        # # self.cache[text] = outputs
+        # # self.cache_keys.append(text)
+        
+        kw_dict = mydep_dict(content) # already sorted
+        kws = " ".join(list(kw_dict.keys())[:self.n_kws] ) # get top n keywords
+        kws = kws.replace(".", " ")
+        kws_is = set(self.tokenizer.encode(kws)[1:-1]) # remove begin_tok, end_tok
+        kws_texts = [self.tokenizer.ids_to_tokens[kwi] for kwi in kws_is]
+        outputs = (kws_is, kws_texts)
+
+
+        return outputs
+
+class KeywordCoverage():
+    #def __init__(self, device, keyword_model_file, model_file=None, n_kws=15):
+    def __init__(self, device, model_file=None, n_kws=15):
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.vocab_size = self.tokenizer.vocab_size
+        self.n_kws = n_kws
+
+        self.mask_id = 103
+        self.sep_id  = 102
+
+        self.kw_ex = KeywordExtractor(n_kws=self.n_kws)
+    #    self.kw_ex.reload(keyword_model_file)
+        self.model = BertForMaskedLM.from_pretrained("bert-base-uncased")
+        self.device = device
+        self.model.to(self.device)
+        if model_file is not None:
+            print("Model:", "[{}]".format(model_file.split('/')[-1]), "is loaded.")
+            self.reload_model(model_file)
+
+    def print_keywords(self, content):
+        kws_is, kws_texts = self.kw_ex.extract_keywords(content)
+        print(kws_texts)
+
+    def mask_text(self, text_tokenized, content):
+        kws_is, kws_texts = self.kw_ex.extract_keywords(content)
+        kws_is = set(kws_is)
+        # unmasked = self.tokenizer.encode(text)
+        masked = [self.mask_id if wi in kws_is else wi for wi in text_tokenized]
+        return masked
+
+    def reload_model(self, model_file):
+        print(self.model.load_state_dict(torch.load(model_file), strict=False))
+
+    def save_model(self, model_file):
+        torch.save(self.model.state_dict(), model_file)
+
+    def build_io(self, contents_tokenized, contents, summaries):
+        N = len(contents_tokenized)
+        maskeds, unmaskeds = [], []
+        summ_toks = []
+        T1 = time.time()
+        for content_tokenized, content, summary in zip(contents_tokenized, contents, summaries):
+            masked = self.mask_text(content_tokenized, content) # .decode()
+            maskeds.append(torch.LongTensor(masked))
+            unmaskeds.append(torch.LongTensor(content_tokenized))
+            summ_toks.append(torch.LongTensor(self.tokenizer.encode(summary))) # .decode()
+        T2 = time.time()
+        input_ids = torch.nn.utils.rnn.pad_sequence(maskeds, batch_first=True, padding_value=0)
+        labels = torch.nn.utils.rnn.pad_sequence(unmaskeds, batch_first=True, padding_value=0)
+        input_ids = input_ids[:, :300]
+        labels = labels[:, :300]
+
+        summ_toks = torch.nn.utils.rnn.pad_sequence(summ_toks, batch_first=True, padding_value=0)
+        summ_toks = summ_toks[:, :100]
+        T3 = time.time()
+        seps = torch.LongTensor([self.sep_id] * N).unsqueeze(1)
+        input_ids = torch.cat((summ_toks, seps, input_ids), dim=1)
+        labels = torch.cat((summ_toks, seps, labels), dim=1)
+        is_masked = input_ids.eq(torch.LongTensor([self.mask_id])).long()
+
+        # Make the labels classifier friendly
+        labels = labels * is_masked + (1-is_masked) * torch.LongTensor([-1])
+
+        T4 = time.time()
+        # print(T2-T1, T3-T2, T4-T3)
+        labels = labels.to(self.device)
+        input_ids = input_ids.to(self.device)
+        is_masked = is_masked.to(self.device)
+
+        return input_ids, is_masked, labels
+
+    def train_batch(self, contents, summaries):
+        contents_tokenized = [self.tokenizer.encode(cont) for cont in contents]
+
+        input_ids, is_masked, labels = self.build_io(contents_tokenized, contents, summaries)
+
+        outputs, = self.model(input_ids)
+        cross_ent = CrossEntropyLoss(ignore_index=-1)
+        loss = cross_ent(outputs.view(-1, self.vocab_size), labels.view(-1))
+
+        num_masks = torch.sum(is_masked, dim=1).float() + 0.1
+        with torch.no_grad():
+            preds = torch.argmax(outputs, dim=2)
+            accs = torch.sum(preds.eq(labels).long() * is_masked, dim=1).float() / num_masks
+        return loss, accs.mean().item()
+
+    def score(self, summaries, contents, bodies_tokenized, lengths=None, extra=None):
+        contents_tokenized = bodies_tokenized
+        # self.model.eval()
+        with torch.no_grad():
+            input_ids_w, is_masked_w, labels_w = self.build_io(contents_tokenized, contents, summaries)
+            outputs_w, = self.model(input_ids_w)
+            preds_w = torch.argmax(outputs_w, dim=2)
+            num_masks_w = torch.sum(is_masked_w, dim=1).float() + 0.1
+            accs_w = torch.sum(preds_w.eq(labels_w).long() * is_masked_w, dim=1).float() / num_masks_w
+
+            if extra is not None:
+                accs_wo = extra # We're in the argmax, and this has already been computed in the sampled
+            else:
+                input_ids_wo, is_masked_wo, labels_wo = self.build_io(contents_tokenized, contents, [""] * len(contents_tokenized))
+                outputs_wo, = self.model(input_ids_wo)
+                preds_wo = torch.argmax(outputs_wo, dim=2)
+                num_masks_wo = torch.sum(is_masked_wo, dim=1).float() + 0.1
+                accs_wo = torch.sum(preds_wo.eq(labels_wo).long() * is_masked_wo, dim=1).float() / num_masks_wo
+        score = accs_w - accs_wo
+        return score.tolist(), accs_wo
+
+if __name__ == "__main__":
+    import utils_tokenizer
+
+    contents = ["To the chagrin of New York antiques dealers, lawmakers in Albany have voted to outlaw the sale of virtually all items containing more than small amounts of elephant ivory, mammoth ivory or rhinoceros horn. The legislation, which is backed by Gov. Andrew M. Cuomo, will essentially eliminate New York's central role in a well-established, nationwide trade with an estimated annual value of $500 million. Lawmakers say the prohibitions are needed to curtail the slaughter of endangered African elephants and rhinos, which they say is fueled by a global black market in poached ivory, some of which has turned up in New York. The illegal ivory trade has no place in New York State, and we will not stand for individuals who violate the law by supporting it,\" Mr. Cuomo said in a statement on Tuesday, during the debate on the bill. The bill was approved by the Assembly on Thursday, 97 to 2, and passed the Senate, 43 to 17, on Friday morning. Mr. Cuomo is expected to sign it within a week. Assemblyman Robert K. Sweeney, Democrat of Lindenhurst, a sponsor, said that the law \"recognizes the significant impact our state can have on clamping down on illegal ivory sales\" and that it would help rescue elephants from \"ruthless poaching operations run by terrorists and organized crime.\" Dealers and collectors who trade in ivory antiques owned long before the era of mass poaching say the restrictions, which are stiffer than similar federal rules announced in May, will hurt legitimate sellers but do little to protect endangered animals. The real threat to elephants and rhinos, they say, comes from the enormous illicit market in tusks and horns based in China and other Asian nations. \"It is masterful self-deception to think the elephant can be saved by banning ivory in New"]
+    summaries = ["In selling elephant ivory is illegal."]
+
+    kw_cov = KeywordCoverage("cuda", n_kws=5) #, model_file=model_file, keyword_model_file=os.path.join(models_folder, "keyword_extractor.joblib"))
+    bert_tokenizer = utils_tokenizer.BERTCacheTokenizer()
+
+    contents_tokenized = [bert_tokenizer.encode(body) for body in contents]
+    scores, no_summ_acc = kw_cov.score(summaries, contents, contents_tokenized)
+
+    for body, score, ns_score in zip(contents, scores, no_summ_acc):
+        print("-----------------------------")
+        print("-----------------------------")
+        print("---------- content ----------")
+        print(body)
+        print("---------- summary ----------")
+        print(summaries[0])
+        print("---------- keyword ----------")
+        kw_cov.print_keywords(body)
+        print("-----------------------------")
+        print("-----------------------------")
+        print("--- summary coverage score =  ---")
+        print(score) # score == accs_w - accs_wo
+        print("-- empty doc coverage score --")
+        print(ns_score.item()) # ns_score == accs_wo
+        
---
+++ util_mydep.py
@@ -0,0 +1,314 @@
+import spacy
+import networkx as nx 
+from spacy.lang.en.stop_words import STOP_WORDS
+nlp = spacy.load("en_core_web_sm")
+
+def sort_dict_by_value(input_dict):
+    return dict(sorted(input_dict.items(), key=lambda k: k[1], reverse=True))
+
+def load_stop_words(stopwords=""):
+    stop_words = []
+    for word in STOP_WORDS.union(set(stopwords.split(" "))):
+        stop_words.append(word)
+    return stop_words
+
+def ann_text(text, all_dep=False):
+    #no_pronoun_text = remove_pronoun(text)
+    #doc = nlp(no_pronoun_text)
+    doc = nlp(text)
+    dep_list = []
+    for sent_idx, sent in enumerate(doc.sents):
+        for tok_idx, tok in enumerate(sent):
+            dep = find_dep(tok, all_dep)
+            if dep is not None:
+                dep_list.extend( dep )
+    if all_dep is False:
+        merge_compound_word(dep_list, doc)
+#         print("===== next semt =====")
+    return dep_list
+
+def dep_fwfw(tok, dp1, dp2, double_dir=False, reverse=False): # dep_forward_forward
+    # A->B->C  => C->A
+    #     ex: table for food
+    #         dep = (table-->for), (for-->food)
+    #             => table<--food
+    source = None
+    target = None
+    for child in tok.children:
+        if child.dep_ == dp1:
+            for cchild in child.children:
+                if cchild.dep_ == dp2:
+                    source = tok
+                    target = cchild
+    return creat_dep(source, target, double_dir, reverse)
+
+def dep_side(tok, dp1, dp2, double_dir=False, reverse=False):
+    # A<-B->C => C->A
+    #     ex: He is nice.   dep = (is->he), (is->nice) 
+    #                           => nice->he
+    source = None
+    target = None
+    for child in tok.children:
+        if child.dep_ == dp1:
+            target = child
+        if child.dep_ == dp2:
+            source = child
+    return creat_dep(source, target, double_dir, reverse)
+
+def dep_rem(tok, dp, double_dir=False, reverse=False): # dep_remain
+    # A->B => A->B
+    #     ex: find->it     dep = (find->it)
+    #                          => find->it
+    source = None
+    target = None
+    for child in tok.children:
+        if child.dep_ == dp:
+            source = tok
+            target = child
+    return creat_dep(source, target, double_dir, reverse)
+    
+    
+def creat_dep(source, target, double_dir=False, reverse=False):
+    if source is not None and target is not None:
+        if double_dir:
+            return [( tok2node(source), tok2node(target) ), 
+                    ( tok2node(target), tok2node(source) )]
+        else:
+            if reverse: # source -> target
+                return (tok2node(target), tok2node(source) )
+            else:
+                return (tok2node(source), tok2node(target) )
+    else:
+        return None
+
+def tok2node(token): # token => str( lemma=pos )
+    if type(token) is str:
+        return token
+    else:
+        tok_lemma = token.lemma_
+        if token.lemma_ == "-PRON-":
+            tok_lemma = token.text.lower()
+        if token.pos_ == "PUNCT":
+            return token.text
+        else:
+            return tok_lemma
+    #        return "".join([tok_lemma, '=', token.pos_])
+
+def merge_compound_word(dep_list, doc):
+    compound_word_list, all_dep_in_cpw_list = find_cpw(doc)
+    
+    cpw_dict = dict()
+    for cpw in compound_word_list:
+        main_cpw = cpw[0]
+        part_cpw = cpw[1]
+        cpw_dict[part_cpw] = main_cpw
+    
+    for idx, dep in enumerate(dep_list):
+        source = dep[0]
+        target = dep[1]
+        if source in cpw_dict:
+            dep_list[idx] = (cpw_dict[source], target)
+        elif target in cpw_dict:
+            dep_list[idx] = (source, cpw_dict[target])
+    for dep_in_cpw in all_dep_in_cpw_list:
+        dep_list.extend(dep_in_cpw)
+
+def find_cpw(doc):
+    compound_word_list = []
+    all_dep_in_cpw_list = []
+    tmp_cpw_word = []
+    cpw_head = ""
+    for i in doc:
+        if i.dep_ == "compound":
+            cpw_head = i.head.text
+            if cpw_head not in tmp_cpw_word:
+                tmp_cpw_word.append(i.text)
+        else:
+            if i.text == cpw_head:
+                tmp_cpw_word.append(cpw_head)
+                cpw, cpw_dep_list = dep_in_cpw(tmp_cpw_word)
+                compound_word_list.append( (cpw, i.text) )
+                all_dep_in_cpw_list.extend(cpw_dep_list)
+                tmp_cpw_word = []
+    return compound_word_list, all_dep_in_cpw_list
+
+def dep_in_cpw(tmp_cpw_word):
+    cpw = " ".join(tmp_cpw_word)
+    cpw_dep_list = []            # Robin Kuo <-> Robin
+    for t in tmp_cpw_word:       #           <-> Kuo
+        cpw_dep_list.append(creat_dep(cpw, t, double_dir=True))
+    return cpw, cpw_dep_list
+
+def find_dep(tok, all_dep=False):
+    tmp_dep_list = []
+    if all_dep:
+        tmp_dep_list.append( deps(tok))
+    else:
+        tmp_dep_list.append( nsubj(tok) )
+        tmp_dep_list.append( nsubjp(tok))
+        tmp_dep_list.append( same(tok)  )
+        tmp_dep_list.append( ppobj(tok) )
+        tmp_dep_list.append( agpbj(tok) )
+        tmp_dep_list.append( dative(tok))
+        tmp_dep_list.append( ppcomp(tok))
+        tmp_dep_list.append( advcl(tok) )
+        tmp_dep_list.append(npadvmod(tok))
+        tmp_dep_list.append( amod(tok)  )
+        tmp_dep_list.append( advmod(tok))
+        tmp_dep_list.append( dobj(tok)  )
+        tmp_dep_list.append( nummod(tok))
+        tmp_dep_list.append( xcomp(tok) )
+        tmp_dep_list.append( ccomp(tok) )
+        tmp_dep_list.append( acl(tok)   )
+        tmp_dep_list.append( poss(tok)  )
+        tmp_dep_list.append( relcl(tok) )
+        tmp_dep_list.append( oprd(tok)  )
+        #tmp_dep_list.append(compound(tok))
+    return flat_rmNone_list(tmp_dep_list)
+
+def flat_rmNone_list(tmp_dep_list):
+    dep_list = []
+    for dep in tmp_dep_list:
+        if dep is None:
+            continue
+        elif type(dep) is list:
+            dep_list.extend(dep)
+        else:
+            dep_list.append(dep)
+    return dep_list
+
+def deps(tok):
+    source = None
+    target = None
+    dep_list = []
+    for child in tok.children:
+        source = tok
+        target = child
+        dep_list.append(creat_dep(source, target))
+    return flat_rmNone_list(dep_list)
+
+def nsubj(tok):
+    dep = dep_side(tok, 'nsubj', 'acomp') # "I am nice"
+    if dep is None:
+        dep = dep_side(tok, 'nsubj', 'attr', double_dir=True)  #"Tom is man"
+    if dep is None:
+        dep = dep_rem(tok, 'nsubj') # "I ran home"
+    return dep
+def nsubjp(tok):
+    return dep_rem(tok, 'nsubjpass') # "dog was found"
+def same(tok):
+    return dep_rem(tok, 'appos', double_dir=True) # "Sam, the VIP"
+def agpbj(tok): # agent+pobj
+    return dep_fwfw(tok, 'agent', 'pobj') # "taken by us"
+def ppobj(tok): # prep+pobj
+    if tok.pos_ in ['NOUN', 'PROPN']: 
+        return dep_fwfw(tok, 'prep', 'pobj', reverse=True) # "table of picnic"
+    else:                             
+        return dep_fwfw(tok, 'prep', 'pobj') # "some of toys"
+def dative(tok):
+    return dep_rem(tok, 'dative') # "gave me book"
+def ppcomp(tok):
+    return dep_fwfw(tok, 'prep', 'pcomp') # "play at flying"
+def advcl(tok):
+    return dep_rem(tok, 'advcl') #"cry when fail"
+def npadvmod(tok):
+    return dep_rem(tok, 'npadvmod', reverse=False) # "done this morning"
+def amod(tok):
+    return dep_rem(tok, 'amod', reverse=True) # "poor student"
+def advmod(tok):
+    return dep_rem(tok, 'advmod', reverse=True) # "less often"
+def dobj(tok):
+    return dep_rem(tok, 'dobj') # "find it"
+def nummod(tok):
+    return dep_rem(tok, 'nummod', reverse=True) # "ten books"
+def xcomp(tok):
+    return dep_rem(tok, "xcomp") # "easy to play"
+def ccomp(tok):
+    return dep_rem(tok, "ccomp") # "I conside him fool"
+def poss(tok):
+    return dep_rem(tok, "poss", reverse=True) # "his gun"
+def relcl(tok):
+    return dep_rem(tok, "relcl", reverse=True) # "girl who likes me"
+def oprd(tok):
+    return dep_rem(tok, "oprd") # "made public"
+def acl(tok):
+    return dep_rem(tok, "acl", reverse=True) # "fact that nobody care"
+def compound(tok):
+    return dep_rem(tok, "compound", double_dir=True)
+
+
+def mydep_node_dict(text, direct=True, in_edge=True, show_graph=False, all_dep=False, div_wcnt=False):
+    dep_list = ann_text(text, all_dep=all_dep)
+
+    if direct:
+        G = nx.DiGraph()
+    else:
+        G = nx.MultiGraph() 
+    for i in dep_list: # [('cool=JJ', 'he=NN', 'nsubj')]
+        G.add_edge(i[0], i[1]) 
+    if show_graph:
+        nx.draw_networkx(G, with_label = True, node_color="yellow")
+    
+    # ======= calculate weight ======= 
+    # count how many edge attach to each node
+    node_edge_cnt_dict = dict()
+    for node in G.nodes:
+        if direct==True and in_edge==True:
+            node_edge_cnt_dict[node] = len(G.in_edges(node))
+        else: # out_edge
+            node_edge_cnt_dict[node] = len(G.edges(node))
+
+    node_word_cnt_dict = word_freq_cnt(dep_list)
+
+
+    weight_node_dict = dict()
+    for node in node_edge_cnt_dict.keys():
+        if div_wcnt:
+            weight_node_dict[node] = node_edge_cnt_dict[node] /node_word_cnt_dict[node]
+        else:
+            weight_node_dict[node] = node_edge_cnt_dict[node]
+    return sort_dict_by_value(weight_node_dict)
+
+def word_freq_cnt(dep_list):
+    freq_dict = {}
+    for node_s, node_t in dep_list:
+        if node_t not in freq_dict:
+            freq_dict[node_t] = 1
+        else:
+            freq_dict[node_t] += 1
+
+        if node_s not in freq_dict:
+            freq_dict[node_s] = 1
+        else:
+            freq_dict[node_s] += 1
+    return freq_dict
+
+def mydep_score(text_list, all_dep=False, keystr=False):
+    mydep_list = []
+    for text in text_list:
+        keyword_dict = mydep_dict(text, all_dep=all_dep, keystr=keystr)       
+        mydep_list.append( keyword_dict )
+    return mydep_list
+
+def mydep_dict(text, all_dep=False, keystr=False):
+    node_dict = mydep_node_dict(text, all_dep=all_dep)
+    stopw = load_stop_words()
+
+    keyword_dict = dict()
+
+    ##############################################
+    # this is cheating... but get a better score #
+    keystr = False 
+    ##############################################
+    if keystr:
+        for node in node_dict:
+            if len(node.split(" ")) == 1:
+                kw = node.lower()
+                if kw not in stopw:
+                    keyword_dict[kw] = node_dict[node]   
+    else:
+        for node in node_dict:
+            kw = node.lower()
+            if kw not in stopw:
+                keyword_dict[kw] = node_dict[node]    
+    return sort_dict_by_value(keyword_dict)
