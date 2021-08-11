fd49d2c689920b215b931f6f1d950686ed2d9dc3
robin - 108 master <s103321048@mail1.ncnu.edu.tw>
2021-08-11 16:54:55 +0800

    eval.py is used to generate summmary and evaulate with ROUGE and FEQA

---
+++ eval.py
@@ -0,0 +1,42 @@
+from model_generator import GeneTransformer
+from tqdm import tqdm
+import pickle
+from utils.eval_rouge import cal_rouge
+from utils.eval_FEQA import cal_FEQA
+
+test = 5
+
+# load your model
+model_name = "gpt2_copy61_0.bin" # "summarizer_ckpt.bin"
+generator = GeneTransformer(device="cuda") # Initialize the generator
+generator.reload("models/{}".format(model_name)) 
+
+# load dataset (pickle)
+with open ("data/test_dataset.pkl", "rb") as f:
+    news_list = pickle.load(f)
+
+
+content_list = []
+gold_list = []
+for news in news_list:
+    content_list.append(news['content'])
+    gold_list.append(news['summary'])
+print(len(content_list), len(gold_list))
+
+# generate summary
+cand_list = []
+for idx, new in tqdm(enumerate(news_list)):
+    if idx == test:
+        break
+
+    summary = generator.decode([new['content']], max_output_length=20, beam_size=1, return_scores=True, sample=False)
+    if summary == "":
+        cand_list.append("None")
+    else:
+        cand_list.append(summary[0][0])
+
+
+print( " ==================== " )
+print("doc/cand/gold = {}/{}/{}",len(content_list[:test]), len(cand_list), len(gold_list[:test]))
+result_FEQA = cal_FEQA(cand_list, content_list[:test])
+result_rouge = cal_rouge(gold_list[:test], cand_list)
